4ba938d9020f9d5c8123bb00a2c5acc4ba38fc5e
tung1883 <100570223+tung1883@users.noreply.github.com>
2022-06-05 23:45:09 +0700

    update home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,55 +7,8 @@ const Home = () => {
     const { state, dispatch } = useContext(AppContext)
 
     return (
-        // state.account ? (
-        //     <div className="homeAction">
-        //         <ul className="actionList">
-        //             <li className="dropdown">
-        //                 <div className="dropbtn">chuyên viên</div>
-        //                 <div className="dropdown-content">
-        //                     <a href="#">Danh sách chuyên viên</a>
-        //                     <div>Chỉnh sửa tài khoản chuyên viên</div>
-        //                 </div>
-        //             </li>
-
-        //             <li className="dropdown">
-        //                 <div className="dropbtn">cơ sở kinh doanh</div>
-        //                 <div className="dropdown-content">
-        //                     <a href="#">Danh sách các cơ sở kinh doanh</a>
-        //                     <div>Chỉnh sửa cơ sở kinh doanh</div>
-        //                 </div>
-        //             </li>
-
-        //             <li className="dropdown">
-        //                 <div className="dropbtn">khu vực</div>
-        //                 <div className="dropdown-content">
-        //                     <a href="#">Danh sách các khu vực</a>
-        //                     <div>Chỉnh sửa khu vực</div>
-        //                 </div>
-        //             </li>
-
-        //             <li className="dropdown">
-        //                 <div className="dropbtn">thanh tra</div>
-        //                 <div className="dropdown-content">
-        //                     <a href="#">Lên lịch thanh tra</a>
-        //                     <a href="#">Xem lịch thanh tra</a>
-        //                 </div>
-        //             </li>
-        //         </ul>
-        //     </div>
-        // ) : (
-        //     <div className="middle">
-        //       <section className="intro">
-        //         <h1>Trang web quản lý cơ sở thực phẩm hàng đầu</h1>
-        //         <p>Tiện lợi, nhanh chóng, linh hoạt</p>
-        //       </section>
-        //       <section>
-        //         <Login />
-        //       </section>
-        //     </div>
-        // )   
-
-        <div className="homeAction">
+        state.account ? (
+            <div className="homeAction">
                 <ul className="actionList">
                     <li className="dropdown">
                         <div className="dropbtn">chuyên viên</div>
@@ -90,6 +43,17 @@ const Home = () => {
                     </li>
                 </ul>
             </div>
+        ) : (
+            <div className="middle">
+              <section className="intro">
+                <h1>Trang web quản lý cơ sở thực phẩm hàng đầu</h1>
+                <p>Tiện lợi, nhanh chóng, linh hoạt</p>
+              </section>
+              <section>
+                <Login />
+              </section>
+            </div>
+        )   
     )
 }
 
