4086fcd270c733978260f285567e5f86b71718a5
tung1883 <100570223+tung1883@users.noreply.github.com>
2022-06-06 18:03:40 +0700

    Update Account.js

--- src/features/Accounts/Account.js
+++ src/features/Accounts/Account.js
@@ -6,10 +6,36 @@ import './Account.css';
 
 const Account = () => {
     const { state, dispatch } = useContext(AppContext);
-    
+
     return (
-        state.role === "admin" ? (
-            <form className="form">
+        // state.role === "admin" ? (
+        //     <form className="form">
+        //         <label htmlFor="username">Tên chuyên viên:</label>
+        //         <br />
+        //         <input type="text" name="username" />
+        //         <br />
+        //         <label htmlFor="area">Khu vực:</label>
+        //         <select name="area">
+        //             <option value={'caugiay'}>Cầu Giấy</option>
+        //             <option value={'thanhxuan'}>Thanh Xuân</option>
+        //             <option value={'hoankiem'}>Hoàn Kiếm</option>
+        //             <option value={'tayho'}>Tây Hồ</option>
+        //             <option value={'hoangmai'}>Hoàng Mai</option>
+        //             <option value={'null'}>Chưa được đăng ký khu vực</option>
+        //         </select>
+        //         <br />
+        //         <label htmlFor="isActive">Trạng thái:</label>
+        //         <select name="isActive">
+        //             <option value={true}>Đang hoạt động</option>
+        //             <option value={false}>Không còn hoạt động</option>
+        //         </select>
+        //         <br />
+        //         <button type="submit">Cập nhật</button>
+        //     </form>
+        // ) : (
+        //     <Navigate to='/' push/>
+        // )
+        <form className="form">
                 <label htmlFor="username">Tên chuyên viên:</label>
                 <br />
                 <input type="text" name="username" />
@@ -32,9 +58,6 @@ const Account = () => {
                 <br />
                 <button type="submit">Cập nhật</button>
             </form>
-        ) : (
-            <Navigate to='/' push/>
-        )
     );
 }
 
