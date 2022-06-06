71caff77df44b718ad7c24c7e12c7a03c9034b11
tung1883 <100570223+tung1883@users.noreply.github.com>
2022-06-06 08:25:04 +0700

    Thêm Navigate lại Home nếu không phải admin

--- src/features/Accounts/AccountList/AccountList.js
+++ src/features/Accounts/AccountList/AccountList.js
@@ -1,14 +1,14 @@
 import { React, useState, useCallback, useContext, useEffect } from 'react';
 import AppContext from '../../../components/AppContext/AppContext';
 import axios from '../../../api/axios';
-import { useNavigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import './AccountList.css';
 
 const ACCOUNT_URL = '/admin/accounts';
 
 const AccountList = () => {
-    // KIỂM TRA TÀI KHOẢN CÓ PHẢI ADMIN KHÔNG -> KHÔNG THÌ NAVIGATE LẠI HOME
     const { state, dispatch } = useContext(AppContext);
+    const location = useLocation();
 
     // QUERY
     const [query, setQuery] = useState({ role: '', isActive: '', area: '' });
@@ -62,61 +62,66 @@ const AccountList = () => {
     }, [getAllAccounts, accounts]);
 
     return (
-        <>
-            <h1 className="listName">quản lý người dùng</h1>
-            <table className="list">
-                <tr>
-                    <th>STT</th>
-                    <th>Tên tài khoản</th>
-                    <th>Trạng thái</th>
-                    <th>Khu vực</th>
-                    <th>Xóa tài khoản</th>
-                    <th>Chỉnh sửa</th>
-                </tr>
+        state.role==='admin' ? (
+            <>
+                <h1 className="listName">quản lý người dùng</h1>
+                <table className="list">
+                    <tr>
+                        <th>STT</th>
+                        <th>Tên tài khoản</th>
+                        <th>Trạng thái</th>
+                        <th>Khu vực</th>
+                        <th>Xóa tài khoản</th>
+                        <th>Chỉnh sửa</th>
+                    </tr>
 
-                {accounts ? (
-                    users.map((account, key) => {
-                        return (
-                            <tr>
-                                <td>{key + 1}</td>
-                                <td>{account.username}</td>
-                                <td>
-                                    {account.isActive
-                                        ? 'Hoạt động'
-                                        : 'Không hoạt động'}
-                                </td>
-                                <td>
-                                    {account.area
-                                        ? account.area
-                                        : 'Chưa được đăng ký khu vực'}
-                                </td>
-                                <td>
-                                    <button
-                                        type="submit"
-                                        value={account._id}
-                                        onClick={(e) => handleDelete(e)}
-                                    >
-                                        Xóa
-                                    </button>
-                                </td>
-                                <td>
-                                    <button
-                                        value={account.username}
-                                        type="submit"
-                                    >
-                                        <a href={`users/${account._id}`}>
-                                            Chỉnh sửa
-                                        </a>
-                                    </button>
-                                </td>
-                            </tr>
-                        );
-                    })
-                ) : (
-                    <p>Không có dữ liệu</p>
-                )}
-            </table>
-        </>
+                    {accounts ? (
+                        users.map((account, key) => {
+                            return (
+                                <tr>
+                                    <td>{key + 1}</td>
+                                    <td>{account.username}</td>
+                                    <td>
+                                        {account.isActive
+                                            ? 'Hoạt động'
+                                            : 'Không hoạt động'}
+                                    </td>
+                                    <td>
+                                        {account.area
+                                            ? account.area
+                                            : 'Chưa được đăng ký khu vực'}
+                                    </td>
+                                    <td>
+                                        <button
+                                            type="submit"
+                                            value={account._id}
+                                            onClick={(e) => handleDelete(e)}
+                                        >
+                                            Xóa
+                                        </button>
+                                    </td>
+                                    <td>
+                                        <button
+                                            value={account.username}
+                                            type="submit"
+                                        >
+                                            <a href={`users/${account._id}`}>
+                                                Chỉnh sửa
+                                            </a>
+                                        </button>
+                                    </td>
+                                </tr>
+                            );
+                        })
+                    ) : (
+                        <p>Không có dữ liệu</p>
+                    )}
+                </table>
+            </>
+        ) : (
+            <Navigate to="/" state={{ from: location }} replace />
+        )
+        
     );
 };
 
