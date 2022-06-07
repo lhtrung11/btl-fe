c0a1359297fbe5e10cf5faf9849000df0d2fb2c7
tung1883 <100570223+tung1883@users.noreply.github.com>
2022-06-07 22:16:44 +0700

    update inspection.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Header from './components/header/Header';
+import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import AppContext from './components/AppContext/AppContext';
 import NotFound from './components/NotFound/NotFound';
