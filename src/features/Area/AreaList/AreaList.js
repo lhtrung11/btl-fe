1872c5538ac9e88c8271ba212b26c57474758d74
tung1883 <100570223+tung1883@users.noreply.github.com>
2022-06-07 22:47:34 +0700

    update AreaList

--- src/features/Area/AreaList/AreaList.js
+++ src/features/Area/AreaList/AreaList.js
@@ -1,53 +1,88 @@
 import React from 'react'
-import { useState, useCallback, useEffect } from 'react';
-import { useNavigate } from 'react-router';
+import { useState, useCallback, useEffect, useContext } from 'react';
+import { Navigate } from 'react-router';
 import axios from '../../../api/axios';
-import "./AreaList.css"
+import AppContext from '../../../components/AppContext/AppContext';
+import "./AreaList.css";
+
 export default function AreaList() {
-  const [area, setArea] = useState([]);
-  let navigate = useNavigate();
+  const { state, dispatch } = useContext(AppContext);
+  const [areas, setAreas] = useState([]);
+
   const getAreaList = useCallback(async () => {
     try {
         const token = localStorage.getItem('token');
         const option = {
             method: 'get',
-            url: '/areas',
+            url: '/admin/areas',
             headers: {
                 Authorization: `Bearer ${token}`,
             },
         };
         const response = await axios(option);
-        const area = response.data.data.area;
-        if (area) {
-            setArea(area);
+        const areas = response.data.data.areas;
+
+        if (areas) {
+            setAreas(areas);
         }
     } catch (error) {}
-  }, [setArea]);
+  }, [setAreas]);
 
   useEffect(() => {
     getAreaList();
   }, [getAreaList]);
+
+  const handleDelete = async (e) => {
+        const token = localStorage.getItem('token');
+        const areaID = e.target.value;
+        const option = {
+            method: 'delete',
+            url: `admin/areas/${areaID}`,
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        };
+
+        await axios(option);
+        setAreas((prevAreas) => {
+            return prevAreas.filter((area) => area._id !== areaID);
+        });
+    };
+
   return (
-    <>
-    <content class="header">Bảng dữ liệu hệ thống khu vực</content>
-    <table class="table">
-        <tr>
-            <th>STT</th>
-            <th>Tên khu vực</th>
-            <th>Trạng thái</th>
-        </tr>
-        <tr>
-            <td>1</td>
-            <td>Tùng</td>
-            <td>Hoạt động</td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>Trung</td>
-            <td>Không hoạt động</td>
-        </tr>
-    </table>
-    </>
+    state?.role === 'admin' ? (
+      <>
+        <h1 className="areaListHeader">Khu vực quản lý</h1>
+        <table className="areaTable">
+            <tr>
+                <th>STT</th>
+                <th>Tên khu vực</th>
+                <th>Trạng thái</th>
+                <th>Xóa</th>
+            </tr>
+            {areas.map((area, index) => {
+              return (
+                  <tr>
+                      <td>{index + 1}</td>
+                      <td>{area.name}</td>
+                      <td>{area.license?.isManaged ? 'Có' : 'Không'}</td>
+                      <td>
+                          <button
+                              type="submit"
+                              onClick={(e) => handleDelete(e)}
+                              value={area._id}
+                          >
+                              Xóa
+                          </button>
+                      </td>
+                  </tr>
+              );})
+            }
+        </table>
+      </>
+    ) : (
+      <Navigate to='/' replace></Navigate>
+    )
   )
 }
 
